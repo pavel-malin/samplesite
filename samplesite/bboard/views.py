28bf11f188a8b054b876f8272c3d100155f2eb90
pavel-malin <kurchevskijpavel@gmail.com>
2019-07-05 16:07:10 +0300

    low level tools Django

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))

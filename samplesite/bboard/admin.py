f02eb99830723927aa500851d4666b8c6c2454c6
pavel-malin <kurchevskijpavel@gmail.com>
2019-07-06 15:00:22 +0300

    new rubric and admin.

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Bb
+from .models import Bb, Rubric
 
 class BbAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'price', 'published')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
 
+admin.site.register(Rubric)
 admin.site.register(Bb, BbAdmin)

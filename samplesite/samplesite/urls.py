0470808717bba15dc2a74f773e1c51ca27a5275e
pavel-malin <kurchevskijpavel@gmail.com>
2019-07-07 13:48:00 +0300

    no error html and news form.py

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from bboard.views import index, by_rubric
+from bboard.views import index, by_rubric, BbCreateView
 
 urlpatterns = [
+    path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('bboard/', index, name='index'),
     path('admin/', admin.site.urls),

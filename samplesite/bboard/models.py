8f21230ffceb1c1d5c2b4b02ba76b7ce99b9d0cd
pavel-malin <kurchevskijpavel@gmail.com>
2019-07-15 23:17:28 +0300

    models.py new class AdvUser.

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name='Товар', unique_for_date='published')
@@ -27,3 +28,8 @@ class Rubric(models.Model):
         verbose_name_plural = 'Рубрики'
         verbose_name = 'Рубрика'
         ordering = ['name']
+
+
+class AdvUser(models.Model):
+    is_activated = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)

8689f1380e48cd48f59b076112a67ae4ad4ea071
pavel-malin <kurchevskijpavel@gmail.com>
2019-07-16 01:08:05 +0300

    models.py new class Spare.

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -33,3 +33,10 @@ class Rubric(models.Model):
 class AdvUser(models.Model):
     is_activated = models.BooleanField(default=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Spare(models.Model):
+     name = models.CharField(max_length=30)
+
+class Machine(models.Model):
+    name = models.CharField(max_length=30)
+    spares = models.ManyToManyField(Spare)

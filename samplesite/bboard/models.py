1e400e03332f6fa9a1b1953ef81da3b35f3c242c
pavel-malin <kurchevskijpavel@gmail.com>
2019-07-20 21:05:22 +0300

    Validators/

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.core import validators
 from django.contrib.auth.models import User
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Товар', unique_for_date='published')
+    title = models.CharField(max_length=50,
+        validators=[validators.RegexValidator(regex='^.{4,}$')],
+        verbose_name='Товар', unique_for_date='published')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')

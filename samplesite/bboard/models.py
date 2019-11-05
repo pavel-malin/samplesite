6dfd0ce07522edb754cdd1de10955c0ec88c7f9a
pavel-malin <kurchevskijpavel@gmail.com>
2019-11-05 22:49:46 +0300

    new blue

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -99,7 +99,7 @@ class MinMaxValueValidator:
     def __call__(self, val):
         if val < self.min_value or val > self.max_value:
             raise ValidationError('Введенное число должно ' +
-                                  'находиться в диапазоне от % (min)s до \
-                                  % (max)s', code='out_of_range',
+                                  'находиться в диапазоне от %(min)s до \
+                                  %(max)s', code='out_of_range',
                                   params={'min': self.min_value,
                                           'max': self.max_value})
